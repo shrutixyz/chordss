1cdd2e81f56c51a5674734fe5fd284aaba6b047b
aakzsh <aakashferrari@gmail.com>
2021-11-29 23:41:13 +0530

    changes

--- src/pages/Left.js
+++ src/pages/Left.js
@@ -10,6 +10,7 @@ const Left = () => {
           <p  class="inline-block text-sm px-4 py-2 leading-none border rounded text-yellow-400 border-yellow-400 hover:border-transparent  hover:bg-yellow-400 hover:text-black mt-4 lg:mt-0">Return to Home</p>
           
           </Link>
+          
            </center>
 
         </div>
